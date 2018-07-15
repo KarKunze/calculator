019f2a8bc0e3744802ea38e5730f768e7ab653d6
Karla Kunze <karlalkunze@gmail.com>
2018-07-15 08:47:14 -0400

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(port, function() {
   console.log('I am listening on port 3000!');
